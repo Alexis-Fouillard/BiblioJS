ab81e096b55f7246f886d61c7d66fd6de6aa365d
Alexis Fouillard <alexis.fouillard@free.fr>
2021-04-30 10:13:12 +0200

    Gestion du Directory book-list:
    - Comprends book-form, single-book,book-list

--- src/app/book-list/book-form/book-form.component.ts
+++ src/app/book-list/book-form/book-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {BooksService} from '../../services/books.service';
+import {Router} from '@angular/router';
+import {Book} from '../../../models/book.model';
 
 @Component({
   selector: 'app-book-form',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookFormComponent implements OnInit {
 
-  constructor() { }
+  bookForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder,
+              private bookService: BooksService,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.initForm();
   }
 
+  initForm(){
+    this.bookForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      author: ['', Validators. required],
+      synopsis: ''
+    });
+  }
+
+  /**
+   * Permet de sauvegarder un livre.
+   */
+  onSaveBook() {
+    const title = this.bookForm.get('title').value;
+    const author = this.bookForm.get('author').value;
+    const synopsis = this.bookForm.get('synopsis').value;
+    const newBook = new Book(title, author);
+    newBook.synopsis = synopsis;
+    this.bookService.createNewBook(newBook);
+    this.router.navigate(['/books']);
+  }
 }
