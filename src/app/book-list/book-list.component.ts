7e10e45a2010baa6bda91b1bb91bbc530a46e16f
Alexis-Fouillardgit config --global user.name Alexis-Fouillardgit config --global user.name Alexis-Fouillard <alexis.fouillard@free.fr>
2021-04-30 09:21:02 +0200

    Création des services:
    - Méthodes pour gérer les livres,
    - Debbugage du headerComponent,
    - Modification du .json
    - Réalisation de la sécurité.

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Book} from '../../models/book.model';
+import {Subscription} from 'rxjs';
+import {BooksService} from '../services/books.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.css']
 })
-export class BookListComponent implements OnInit {
+export class BookListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  books: Book[];
+  booksSubscription: Subscription;
+
+  constructor(private bookService: BooksService,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.booksSubscription = this.bookService.booksSubject.subscribe(
+      (books: Book[]) => {
+        this.books = books;
+      }
+    );
+    this.bookService.emitBooks();
+  }
+
+  /**
+   * Redirige vers la page de création de livre.
+   */
+  onNewBook() {
+    this.router.navigate(['/books', 'new']);
+  }
+
+  /**
+   * Supprime un livre d ela bibliotheque
+   * @param book : Book, Le livre a supprimé.
+   */
+  onDeleteBook(book: Book){
+    this.bookService.removeBook(book);
+  }
+
+  /**
+   * Redirige vers la page view
+   * @param id : number , L'identifiant du livre.
+   */
+  onViewBook(id: number) {
+    this.router.navigate(['/books', 'view', id]);
+  }
+
+  ngOnDestroy() {
+    this.booksSubscription.unsubscribe();
   }
 
 }
--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,9 +1,76 @@
 import { Injectable } from '@angular/core';
+import {Subject} from 'rxjs/Rx';
+import firebase from 'firebase';
+import DataSnapshot = firebase.database.DataSnapshot;
+import {Book} from '../../models/book.model';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class BooksService {
 
-  constructor() { }
+  books: Book[] = [];
+  booksSubject = new Subject<Book[]>();
+
+  emitBooks() {
+    this.booksSubject.next(this.books);
+  }
+
+  /**
+   * Permet de sauvegarder la liste sur un node de la BDD.
+   */
+  saveBooks() {
+    firebase.database().ref('/books').set(this.books);
+  }
+
+  getBooks() {
+    firebase.database().ref('/books')
+      .on('value', (data: DataSnapshot) => {
+        this.books = data.val() ? data.val() : [];
+        this.emitBooks();
+      }
+    );
+  }
+
+  constructor() {
+    this.getBooks();
+  }
+
+  /**
+   * Récupère un livre selon son id. Once ne fait qu'une seule requête de données,
+   * et retourne une promise permettant l'utilisation de .then() pour retourner
+   * les données reçues.
+   * @param id : number , l'index du livre dans la liste.
+   */
+  getSingleBook(id: number) {
+    return new Promise(
+      (resolve, reject) => {
+        firebase.database().ref('/books/'+id).once('value').then(
+          (data: DataSnapshot) => {
+            resolve(data.val());
+          }, (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+  createNewBook(newBook: Book) {
+    this.books.push(newBook);
+    this.saveBooks();
+    this.emitBooks();
+  }
+
+  removeBook(book: Book) {
+    const bookIndexToRemove = this.books.findIndex(
+      (bookE1) => {
+        if (bookE1 === book) {
+          return true;
+        }
+      }
+    );
+    this.books.splice(bookIndexToRemove,1);
+    this.saveBooks();
+    this.emitBooks();
+  }
+
 }
